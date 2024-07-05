75f58f9d80c896a5efaa4a775d9be608afebd071
hadiq <hadiqfawzie@gmail.com>
2024-07-06 00:43:19 +0700

    Penambahan whatsapp lagi

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return ( 
         <>
         <div className="fixed bottom-0 right-0">
-        <Link href="https://wa.me/+6285217298270?text=Hai%20Pak%20Agus,%20Saya%20mau%20pesan">
+        <Link href="https://wa.me/+6285217298270?text=Hai%20Pak%20Agus,%20Saya%20mau%20pesan%20barang">
           <img className="w-20 h-20" src="/wa.svg" alt="wa" />
         </Link>
       </div>     
