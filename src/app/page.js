c5eed6fb2c21d55729189bcd0c04ef3d50c2a1ff
hadiq <hadiqfawzie@gmail.com>
2024-07-13 20:20:51 +0700

    fixed page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import supabase from "../../utils/supabase";
 export const revalidate = 30
 
 export default async function Home() {  
-  const { data : katalog, error } = await supabase.from('katalog').select('').or('peruntukan.eq.desa,peruntukan.eq.all').order('id', { ascending: true });
+  const { data : katalog, error } = await supabase.from('katalog').select('').order('id', { ascending: true });
   
   if (error) {
     console.log(error.message);
@@ -12,7 +12,7 @@ export default async function Home() {
   return (
     <>
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-6 gap-1">
-       {katalog.map((brg,idx) => (          
+       {katalog && katalog.map((brg,idx) => (          
           <Card key={idx} id={brg.id} foto={brg.foto} deskripsi={brg.deskripsi} 
                 bintang={brg.bintang} harga={brg.harga.toLocaleString("id-ID")} diskon={brg.diskon} 
                 hargaAsal={(brg.harga + (brg.harga * brg.diskon / 100)).toLocaleString("id-ID")} 
