1996ad081f6d0bacb45ed522edc735d60ff6fe79
hadiq <hadiqfawzie@gmail.com>
2024-07-16 14:56:28 +0700

    update header

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
         <>
             <nav className="bg-gray-900 dark:bg-gray-900 border-l-gray-200 border-l-4 dark:border-l-gray-200 dark:border-l-4 
                  border-b-purple-300 border-b-4 dark:border-b-purple-300 dark:border-b-4 rounded-bl-2xl">
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto pt-4">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto pt-2">
                     <div className="w-full flex flex-wrap justify-between px-2">
                         <div>
                             <Link href="https://katalog-pbj.vercel.app/">
@@ -16,7 +16,7 @@ const Nav = () => {
                             font-medium">CV Putra Blimbingsari Jaya</div>
                     </div>                   
                 </div>
-                <div className="text-sm text-slate-400 dark:text-sm dark:text-slate-400 italic text-center -mt-1 pb-3">
+                <div className="text-sm text-slate-300 dark:text-sm dark:text-slate-400 italic text-center -mt-1.5 pb-2">
                     Murah - Berkualitas - Full Service
                 </div>
              
--- src/app/components/SupCloudi/CreateKatalog.jsx
+++ src/app/components/SupCloudi/CreateKatalog.jsx
@@ -10,7 +10,7 @@ const CreateKatalog = () => {
     const [deskripsi, setDeskripsi] = useState('')
     const [bintang, setBintang] = useState('')
     const [harga, setHarga] = useState('')
-    const [diskon, setDiskon] = useState(0)
+    const [diskon, setDiskon] = useState('')
     const [foto, setFoto] = useState('')
 
 
@@ -49,7 +49,7 @@ const CreateKatalog = () => {
 
     return (
         <>
-            <div className="w-3/4 md:w-1/2 bg-slate-500 text-white mx-auto rounded-xl shadow-lg px-4 py-4">
+            <div className="w-full md:w-1/2 bg-slate-500 text-white rounded-xl shadow-lg px-2 py-4 mx-auto">
                 <h1 className="text-2xl font-semibold">Input Katalog</h1>
                 <hr />
                 <div>
@@ -60,57 +60,55 @@ const CreateKatalog = () => {
                         </div>
                     </MyContext.Provider>
                     <div className="mt-4">
-                        <label htmlFor="deskripsi" className="block text-gray-700 font-bold mb-2">
+                        <label htmlFor="deskripsi" className=" text-gray-300 text-sm font-semibold">
                             Deskripsi:
-
                             <textarea
                                 type="text" id="deskripsi" className="shadow appearance-none border rounded w-full py-2 px-3 
                                     text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                placeholder="ex: 1121100100"
+                                placeholder="ex: Nama dan jenis barang"
                                 value={deskripsi}
                                 onChange={(e) => { setDeskripsi(e.target.value) }}
                             />
                         </label>
                     </div>
-                    <div className="mt-4">
-                        <label htmlFor="bintang" className="block text-gray-700 font-bold mb-2">
+                    <div className="mt-2">
+                        <label htmlFor="bintang" className="text-gray-300 text-sm font-semibold">
                             Bintang:
-
                             <input
                                 type="text" id="bintang" className="shadow appearance-none border rounded w-full py-2 px-3 
                         text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                placeholder="ex: Nindi"
+                                placeholder="ex: 4.5"
                                 value={bintang}
                                 onChange={(e) => { setBintang(e.target.value) }}
                             />
                         </label>
                     </div>
-                    <div className="mt-4">
-                        <label htmlFor="harga" className="block text-gray-700 font-bold mb-2">
+                    <div className="mt-2">
+                        <label htmlFor="harga" className="block text-gray-300 text-sm font-semibold mb-1">
                             Harga:
+                            <input
+                                type="text" id="harga" className="shadow appearance-none border rounded w-full py-2 px-3 
+                            text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                placeholder="ex: 7300000"
+                                value={harga.replace(/[^0-9]/g, '')}
+                                onChange={(e) => { setHarga(e.target.value) }}
+                            />
                         </label>
-                        <input
-                            type="text" id="harga" className="shadow appearance-none border rounded w-full py-2 px-3 
-                        text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            placeholder="ex: 2021"
-                            value={harga}
-                            onChange={(e) => { setHarga(e.target.value) }}
-                        />
                     </div>
-                    <div className="mt-4">
-                        <label htmlFor="program-studi" className="block text-gray-700 font-bold mb-2">
+                    <div className="mt-2">
+                        <label htmlFor="program-studi" className="block text-gray-300 text-sm font-semibold mb-1">
                             Diskon :
                             <input
                                 type="text" id="harga" className="shadow appearance-none border rounded w-full py-2 px-3 
                         text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                placeholder="ex: 2021"
+                                placeholder="ex: 9"
                                 value={diskon}
                                 onChange={(e) => { setDiskon(e.target.value) }}
                             />
                         </label>
 
                     </div>
-                    <div className="flex flex-row justify-end mt-4">
+                    <div className="flex flex-row justify-end mt-2">
                         <button
                             className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
                             onClick={addKatalog}
